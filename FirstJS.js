429eaf6a9bb323ff35810a377a9d1ada0b54351e
Clarelow <32047240+Clarelow@users.noreply.github.com>
2017-09-20 19:55:01 +0800

    profile_2

---
+++ FirstJS.js
@@ -0,0 +1,13 @@
+//start JS here
+//alert("Welcome to JS class")
+
+var student = "clarissa"
+//create 5 variables
+var student_1 = "Ryan";
+var student_2 = "Dylan";
+var student_3 = "Careby";
+var student_4 = "BaoBao";
+var student_5 = "DD5"
+
+var welcome_msg = "Hai. We are friends.. " +  student_1 + " , " + student_2
+console.log(welcome_msg)
